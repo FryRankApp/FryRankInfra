95c1058645ba85e097c6832720a7b691665f4bca
Nicholas <nicholaspriv@gmail.com>
2025-02-27 21:16:26 -0800

    Creating Fargate stack

--- stack/ecr.tf
+++ stack/ecr.tf
@@ -1,15 +1,3 @@
-locals {
-  name   = "fryrank-app"
-
-  account_id = data.aws_caller_identity.current.account_id
-
-  tags = {
-    Name       = local.name
-    Example    = local.name
-    Repository = "https://github.com/NickPriv/FryRankInfra"
-  }
-}
-
 data "aws_caller_identity" "current" {}
 
 ################################################################################
--- stack/main.tf
+++ stack/main.tf
@@ -3,5 +3,13 @@ provider "aws" {
 }
 
 locals {
+  name   = "fryrank-app"
   region = "us-west-2"
+  account_id = data.aws_caller_identity.current.account_id
+
+  tags = {
+    Name       = local.name
+    Example    = local.name
+    Repository = "https://github.com/NickPriv/FryRankInfra"
+  }
 }
