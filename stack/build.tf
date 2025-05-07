d1c79f7ed79c6910eb4aa44431d4578786b79b64
Nicholas <nicholaspriv@gmail.com>
2025-05-06 21:20:57 -0700

    use Lambda for build compute

--- stack/build.tf
+++ stack/build.tf
@@ -65,9 +65,8 @@ resource "aws_codebuild_project" "lambda_build" {
   }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
-    type                       = "LINUX_CONTAINER"
+    compute_type                = "BUILD_LAMBDA_1GB"
+    type                        = "LINUX_LAMBDA_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
   }
 
