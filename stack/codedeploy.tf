edf06f991fc3bbf27f06f8f031dcc3c69e2548b2
Nicholas <nicholaspriv@gmail.com>
2025-05-29 21:48:28 -0700

    Grant CodeDeploy permission to get the Lambda zip file

--- stack/codedeploy.tf
+++ stack/codedeploy.tf
@@ -25,6 +25,30 @@ resource "aws_iam_role_policy_attachment" "codedeploy_service_role_policy" {
   role       = aws_iam_role.codedeploy_service_role.name
 }
 
+# Add S3 permissions for CodeDeploy
+resource "aws_iam_role_policy" "codedeploy_s3_policy" {
+  name = "${local.name}-codedeploy-s3-policy"
+  role = aws_iam_role.codedeploy_service_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:GetBucketVersioning"
+        ]
+        Resource = [
+          module.fryrank_lambda_function_bucket.s3_bucket_arn,
+          "${module.fryrank_lambda_function_bucket.s3_bucket_arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
 # Create deployment groups for each Lambda function
 resource "aws_codedeploy_deployment_group" "lambda_deployment_groups" {
   for_each = local.lambda_functions
