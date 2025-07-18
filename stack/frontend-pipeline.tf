0529153ff27c1b8dba5b075e56eb7bc86252c26a
Nicholas Privitera <nicholaspriv@gmail.com>
2025-07-17 21:22:55 -0700

    Create frontend pipeline and build infrastructure (#55)
    * Create frontend pipeline and build infrastructure
    
    * Parameterize the resource names

---
+++ stack/frontend-pipeline.tf
@@ -0,0 +1,62 @@
+resource "aws_codepipeline" "frontend" {
+  name     = "${local.name}-frontend-deploy-pipeline"
+  role_arn = aws_iam_role.frontend_codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.spa_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["SourceOutput"]
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.github.arn
+        FullRepositoryId = "FryRankApp/FryRankFrontend"
+        BranchName       = "main"
+        DetectChanges    = "true"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["SourceOutput"]
+      output_artifacts = ["BuildOutput"]
+      version          = "1"
+      configuration = {
+        ProjectName = aws_codebuild_project.frontend_build.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "DeployToS3"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "S3"
+      input_artifacts = ["BuildOutput"]
+      version         = "1"
+      configuration = {
+        BucketName = aws_s3_bucket.spa_bucket.bucket
+        Extract    = "true"
+      }
+    }
+  }
+} 
