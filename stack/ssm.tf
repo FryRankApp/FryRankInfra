b4e55c8cb785734eeb4bd9ea76e05d7dea00e425
Nicholas <nicholaspriv@gmail.com>
2025-08-05 23:03:54 -0700

    Retrieve console-managed SSM parameters instead of managing them in Terraform

--- stack/ssm.tf
+++ stack/ssm.tf
@@ -3,20 +3,17 @@ data "aws_ssm_parameter" "database_uri" {
   with_decryption = false
 }
 
-resource "aws_ssm_parameter" "google_api_key" {
+data "aws_ssm_parameter" "google_api_key" {
   name        = "GOOGLE_API_KEY"
-  type        = "SecureString"
-  value       = var.google_api_key
+  with_decryption = true
 }
 
-resource "aws_ssm_parameter" "google_auth_key" {
+data "aws_ssm_parameter" "google_auth_key" {
   name        = "GOOGLE_AUTH_KEY"
-  type        = "SecureString"
-  value       = var.google_auth_key
+  with_decryption = true
 }
 
-resource "aws_ssm_parameter" "backend_service_path" {
+data "aws_ssm_parameter" "backend_service_path" {
   name        = "BACKEND_SERVICE_PATH"
-  type        = "String"  # Not SecureString since this is typically a URL
-  value       = var.backend_service_path
+  with_decryption = false
 }
