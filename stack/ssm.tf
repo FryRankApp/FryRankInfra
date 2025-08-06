f112108eae29d6eca16573205dbc928eed2ff781
Nicholas Privitera <nicholaspriv@gmail.com>
2025-08-05 23:05:59 -0700

    Create SSM parameters for React Google API Key, Google Auth Key, and Backend Service Path (#60)
    * Create SSM parameters for React Google API Key, Google Auth Key, and Backend Service Path
    
    * Retrieve console-managed SSM parameters instead of managing them in Terraform

--- stack/ssm.tf
+++ stack/ssm.tf
@@ -2,3 +2,18 @@ data "aws_ssm_parameter" "database_uri" {
   name = "DATABASE_URI"
   with_decryption = false
 }
+
+data "aws_ssm_parameter" "google_api_key" {
+  name        = "GOOGLE_API_KEY"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "google_auth_key" {
+  name        = "GOOGLE_AUTH_KEY"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "backend_service_path" {
+  name        = "BACKEND_SERVICE_PATH"
+  with_decryption = false
+}
