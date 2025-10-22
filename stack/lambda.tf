7a62dc5e1aa02ef0547053adb1fcbd9ece94c3dd
Nicholas Privitera <nicholaspriv@gmail.com>
2025-10-21 22:18:30 -0700

    Set reserved concurrency to prevent uncontrolled Lambda scaling on high traffic (#65)
    * Set reserved concurrency to prevent uncontrolled Lambda scaling on high traffic
    
    * Setting concurrency limit to 25
    
    * Use correct name for reserved concurrency

--- stack/lambda.tf
+++ stack/lambda.tf
@@ -113,6 +113,7 @@ resource "aws_lambda_function" "fryrank_api_lambdas" {
   runtime = "java21"
   description = ""
   timeout = 15
+  reserved_concurrent_executions = 25  # Limit total concurrent executions
 
   environment {
     variables = {
