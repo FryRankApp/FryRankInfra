380d4e7db29d0ca77feb7319851290c35ad137dd
Nicholas Privitera <nicholaspriv@gmail.com>
2026-03-19 22:24:42 -0700

    Increasing timeout and memory to reduce likelihood of a timeout (#97)

--- stack/api-gateway.tf
+++ stack/api-gateway.tf
@@ -25,10 +25,6 @@ resource "aws_api_gateway_stage" "fryrank_api_v1" {
   rest_api_id   = aws_api_gateway_rest_api.fryrank_api.id
   stage_name    = "v1"
 
-  variables = {
-    lambdaAlias = local.lambda_alias_name
-  }
-
 }
 
 resource "aws_api_gateway_usage_plan" "fryrank_api_usage_plan" {
